cb2e4aa9a032d0909ddcfa91c9338efc0a183185
Antonio D'souza <adsouza@gmail.com>
2013-11-11 21:51:31 -0500

    go!

---
+++ main.go
@@ -0,0 +1,14 @@
+package hackersguild
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func init() {
+    http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "<h1>Hack the planet!</h1>")
+}
